66cfc93820db48a0a452f348fe656362a6065532
Suzerin <wwwww.stepen@gmail.com>
2024-12-11 19:34:21 +1100

    Functions is avaliable now

---
+++ array.cpp
@@ -0,0 +1,25 @@
+#include "array.h"
+int max(int left,int right){
+    return left > right ? left : right;
+}
+int min(int left,int right){
+    return left < right ? left : right;
+}
+int min_index(int* massive,int size){
+    int min_index = 0;
+    for(int i = 1; i < size; i++){
+        if(massive[i] < massive[min_index]){
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+int max_index(int* massive,int size){
+    int max_index = 0;
+    for(int i = 1; i < size; i++){
+        if(massive[i] > massive[max_index]){
+            max_index = i;
+        }
+    }
+    return max_index;
+}
